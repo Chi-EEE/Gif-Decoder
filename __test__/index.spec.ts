c78335e71bcc99bdad24dfe7a7f6ef6f482f6704
Huu Chi Huynh <73843190+HuuChiHuynh@users.noreply.github.com>
2022-07-23 14:05:33 +0100

    Update index.spec.ts

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -3,31 +3,31 @@ import test from 'ava'
 import { Decoder } from '../index'
 
 test('sample_1.gif: Frame Count is 1', (t) => {
-  let gif = Decoder.decode("./gifs/sample_1.gif");
+  const gif = Decoder.decode('./gifs/sample_1.gif');
   t.is(gif.frames.length, 1)
 })
 
 test('sample_1.gif: Version is 89a', (t) => {
-  let gif = Decoder.decode("./gifs/sample_1.gif");
-  t.is(gif.version, "89a")
+  const gif = Decoder.decode('./gifs/sample_1.gif');
+  t.is(gif.version, '89a')
 })
 
 test('sample_2_animation.gif: Frame Count is 3', (t) => {
-  let gif = Decoder.decode("./gifs/sample_2_animation.gif");
+  const gif = Decoder.decode('./gifs/sample_2_animation.gif');
   t.is(gif.frames.length, 3)
 })
 
 test('sample_2_animation.gif: Frame 3: Top is 2', (t) => {
-  let gif = Decoder.decode("./gifs/sample_2_animation.gif");
+  const gif = Decoder.decode('./gifs/sample_2_animation.gif');
   t.is(gif.frames[2].im.top, 2)
 })
 
 test('Dancing.gif: Global Table Length is 256', (t) => {
-  let gif = Decoder.decode("./gifs/Dancing.gif");
+  const gif = Decoder.decode('./gifs/Dancing.gif');
   t.is(gif.globalTable.length, 256)
 })
 
 test('Dancing.gif: Frame 1: Local Color Table Flag is false', (t) => {
-  let gif = Decoder.decode("./gifs/Dancing.gif");
+  const gif = Decoder.decode('./gifs/Dancing.gif');
   t.is(gif.frames[0].im.localColorTableFlag, false)
 })
